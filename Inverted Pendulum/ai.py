7fe0aa68eb1ced5adb2aacd8e2c03ceec88d2346
Harrison <harrison.chung06@gmail.com>
2026-02-11 12:24:48 -0800

    Add Feature, classes.py, Bounds detection and game reset

---
+++ Inverted Pendulum/ai.py
@@ -0,0 +1,15 @@
+import numpy 
+
+class ai:
+    def __init__(self, x, v_x, a_c, theta, omega, alpha):
+        self.parameters = [x,v_x,a_c,theta,omega,alpha]
+        self.F_in = 0  
+    
+    def decision_tree(self):
+        return self.F_in
+      
+
+
+
+        
+
